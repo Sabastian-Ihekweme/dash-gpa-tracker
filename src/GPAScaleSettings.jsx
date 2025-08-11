8de9c26f0baa15530d3b50690862c3c2bc19c523
Sabastian-Ihekweme <sabastianfabian001@gmail.com>
2025-08-11 05:15:44 +0100

    ui fix

--- src/GPAScaleSettings.jsx
+++ src/GPAScaleSettings.jsx
@@ -24,6 +24,9 @@ function GPAScaleSettings({gpaScale, setGpaScale}) {
                     <option value="5.0">5.0 Scale</option>
                 </select>
             </div>
+
+            <p>{ gpaScale == '5.0' ? 'A: 70-100. B: 60-69. C: 50-59. D: 45-49. E: 40-44. F: 0-39'
+            : 'A: 70-100. B: 60-69. C: 50-59. D: 40-49. F: 0-39'}</p>
         </div>
     </>
     )
